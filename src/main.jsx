43f2a4cec6940e3bbbf5d29b39d736ccb77f471e
Leweke Precious Chigozie <lewekeprecious8@gmail.com>
2026-08-01 13:46:17 +0100

    Update AuthProvider import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './components/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
